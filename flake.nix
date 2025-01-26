18510394ebd9410a3f814b578ad00e1d61ab68fa
Manuel D <manuel_d@live.de>
2025-01-26 16:40:22 +0100

    initial flake config just calling configuration.nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+				description = "Basic Flake importing configuration.nix";
+
+				inputs = {
+								nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+				};
+
+				outputs = { self, nixpkgs }: 
+				let 
+								arch = "x86_64-linux";
+				in
+				{
+								nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+												system = "${arch}";
+												modules = [
+																./system/configuration.nix
+												];
+								};
+				};
+}
