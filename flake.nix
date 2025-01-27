1793c9d28ec61cd259474d2935da6d938e6ee56c
Manuel D <manuel_d@live.de>
2025-01-27 11:09:23 +0100

    wrapped conifguration.nix inside flake.nix

--- flake.nix
+++ flake.nix
@@ -1,20 +1,21 @@
 {
-				description = "Basic Flake importing configuration.nix";
+  description = "Basic Flake importing configuration.nix";
 
-				inputs = {
-								nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-				};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
 
-				outputs = { self, nixpkgs }: 
-				let 
-								arch = "x86_64-linux";
-				in
-				{
-								nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-												system = "${arch}";
-												modules = [
-																./system/configuration.nix
-												];
-								};
-				};
+  outputs =
+    { self, nixpkgs }:
+    let
+      arch = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "${arch}";
+        modules = [
+          ./system/configuration.nix
+        ];
+      };
+    };
 }
--- system/configuration.nix
+++ system/configuration.nix
@@ -5,10 +5,10 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -16,6 +16,7 @@
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.firewall.enable = false;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -52,9 +53,10 @@
 
   # Configure keymap in X11
   services.xserver.xkb = {
-    layout = "us";
+    layout = "eu";
     variant = "";
   };
+
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -75,18 +77,40 @@
     #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.manuel = {
     isNormalUser = true;
     description = "Manuel";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
     packages = with pkgs; [
-    #  thunderbird
+      alacritty
+      bat
+      bazecor
+      celluloid
+      flameshot
+      gparted
+      neovim
+      ripgrep
+      rofi
+      thunderbird
+      tldr
+      trash-cli
+      unzip
+      xclip
+      xfce.thunar
+      zoxide
     ];
   };
+
+  # Config Font:
+  # fonts.fontconfig.enable = true;
+  #nerdfonts
+  fonts.fonts = [
+    pkgs.nerd-fonts.fira-code
+  ];
 
   # Enable automatic login for the user.
   services.xserver.displayManager.autoLogin.enable = true;
@@ -102,45 +126,31 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # Enable flatpak support
+  services.flatpak.enable = true;
+
   # Enable flakes
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-   git
-   vim
-   neovim
-   wget
+    git
+    vim
+    neovim
+    wget
+    nixfmt-rfc-style
+    python3Full
+    #python3Packages.pip
   ];
 
   # Set default editor env variable to vim
   environment.variables.EDITOR = "vim";
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  # If I use flake.nix to call this configuration.nix, it doesn't matter what I have written here (I think at least)
+  system.stateVersion = "unstable"; # Did you read the comment?
 
 }
