922d0ede5b72334d899524593c5635115746fa33
Manuel D <manuel_d@live.de>
2025-12-26 11:26:44 +0100

    add steam

--- config/packages.nix
+++ config/packages.nix
@@ -62,5 +62,11 @@
     python3
     #python3Packages.pip
   ];
+
+  programs.steam = {
+  enable = true;
+  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+};
 }
 
